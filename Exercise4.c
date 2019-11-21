5f8096a5c09d31535a891c47ef7842e834d1abb8
Varun Kudalkar <varun.kudalkar@gmail.com>
2019-11-21 19:09:52 +0530

    Fibonacci series

---
+++ Exercise4.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+int main() {
+
+    // 0 1 1 2 3 5 8 ...
+
+    int a = -1, b = 1, c = 0;
+
+    while (c < 100) {
+        c = a + b;
+        a = b;
+        b = c;
+        printf("%d ", c);
+    }
+    return 0;
+}
